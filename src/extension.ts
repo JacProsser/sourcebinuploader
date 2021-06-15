f76793cf34a02a91e4be0da86c884cfc1d3147c2
JacProsser <jacleighp@gmail.com>
2021-06-15 18:14:11 +0100

    y

--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,16 @@
 import * as vscode from "vscode";
+import { ExtensionContext, window, commands, StatusBarAlignment } from "vscode";
 import { create as createBin } from "sourcebin";
 import linguist from "@sourcebin/linguist/dist/linguist.json";
 import { write as copy } from "clipboardy";
 import { sep } from "path";
 
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: ExtensionContext) {
     const commandId = "bin.upload";
 
     // register command "bin.upload" to upload selection to sourcebin
-    const disposable = vscode.commands.registerCommand(commandId, async () => {
-        const editor = vscode.window.activeTextEditor;
+    const disposable = commands.registerCommand(commandId, async () => {
+        const editor = window.activeTextEditor;
 
         const fileName = editor?.document.fileName.split(sep).reverse()[0];
         const ext = fileName?.split(".").reverse()[0];
@@ -20,7 +21,7 @@ export function activate(context: vscode.ExtensionContext) {
             editor?.document.getText(selection) || editor?.document.getText();
 
         if (!selected) {
-            vscode.window.showErrorMessage("Cannot create Empty Bin.");
+            window.showErrorMessage("Cannot create Empty Bin.");
             return;
         }
 
@@ -48,7 +49,7 @@ export function activate(context: vscode.ExtensionContext) {
             );
             url = short;
         } catch (e) {
-            vscode.window.showErrorMessage(
+            window.showErrorMessage(
                 `Uploading to Bin Failed. Error: ${e.message}`
             );
             return;
@@ -58,24 +59,24 @@ export function activate(context: vscode.ExtensionContext) {
         const copyURL = "Copy URL";
         const message = `Successfully uploaded to Sourcebin. Link: ${url}`;
 
-        vscode.window.showInformationMessage(message, ...[openURL, copyURL]).then(selection => {
+        window.showInformationMessage(message, ...[openURL, copyURL]).then(selection => {
             if (selection === openURL) {
                 vscode.env.openExternal(vscode.Uri.parse(`${url}`));
             }
             else if (selection === copyURL) {
                 try {
                     copy(url);
-                    vscode.window.showInformationMessage(`Successfully copied the URL to your clipboard.`);
+                    window.showInformationMessage(`Successfully copied the URL to your clipboard.`);
                 } catch (e) {
-                    vscode.window.showErrorMessage(`Failed to copy the url. ${e}`);
+                    window.showErrorMessage(`Failed to copy the url. ${e}`);
                 }
             }
         });
     });
 
     // create status bar button as a shortcut
-    const statusBarItem = vscode.window.createStatusBarItem(
-        vscode.StatusBarAlignment.Right,
+    const statusBarItem = window.createStatusBarItem(
+        StatusBarAlignment.Right,
         1000
     );
     statusBarItem.command = commandId;
